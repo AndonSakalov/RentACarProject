fd4760296fdfb208ae863357a5e934bc59382670
AndonSakalov <125073896+AndonSakalov@users.noreply.github.com>
2024-11-29 13:31:11 +0200

    AddBranchActionsAndOptions
    Add layer for branch actions/functions like edit/delete vehicle, list all vehicles due for service and all vehicles rentals that are due for the current day

---
+++ RentACar.Data/Migrations/20241129112817_AddServicedAtPropInVehicleEntityModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RentACar.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddServicedAtPropInVehicleEntityModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ServicedAt",
+                table: "Vehicles",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                comment: "Mileage of the car.");
+
+            migrationBuilder.UpdateData(
+                table: "Vehicles",
+                keyColumn: "Id",
+                keyValue: new Guid("8a6fbd63-0c0e-4b82-a5c5-c50f9de6ec12"),
+                column: "ServicedAt",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Vehicles",
+                keyColumn: "Id",
+                keyValue: new Guid("c59b3736-645b-471b-9c0b-205792c82b8e"),
+                column: "ServicedAt",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Vehicles",
+                keyColumn: "Id",
+                keyValue: new Guid("d9e0f547-3b8e-42a6-a8a5-6c74ec9b0154"),
+                column: "ServicedAt",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Vehicles",
+                keyColumn: "Id",
+                keyValue: new Guid("e6c4c94a-8c92-44f4-a213-7bdbf3e5a57f"),
+                column: "ServicedAt",
+                value: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ServicedAt",
+                table: "Vehicles");
+        }
+    }
+}
